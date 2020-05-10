49f4708a47030c777aed059e0a3ac7341f9f366d
sushmita2305 <65077699+sushmita2305@users.noreply.github.com>
2020-05-10 16:39:39 +0530

    first commit

---
+++ js/customslick.js
@@ -0,0 +1,17 @@
+$(function (){
+//    $('.multiple-items').slick( { 
+//  infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   arrows:false,
+//   autoPlay:true,
+//   autoplaySpeed:2000,
+// });
+    $('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows:false,
+  autoplay: true,
+  autoplaySpeed: 1000,
+    });
+});
